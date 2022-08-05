1532950318c9de0bcdd80d858e1f36b5158c858f
Zoharfri <Zoharfri@gmail.com>
2022-08-05 17:48:31 +0300

    push

--- Assets/_Scripts/dinoLvl2.cs
+++ Assets/_Scripts/dinoLvl2.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class dinoLvl2 : MonoBehaviour
 {
-   
+    [SerializeField] Transform left;
+    [SerializeField] Transform right;
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
-        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
-        RaycastHit hit;
-        if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
-        {
-            transform.position = new Vector3(hit.transform.position.x, transform.position.y, transform.position.z);
-            print("hit: " + hit.transform.position.x);
-        }
-       
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePosition.x < left.transform.position.x)
+        { mousePosition.x = left.transform.position.x;}
+        if (mousePosition.x > right.transform.position.x)
+        {mousePosition.x = right.transform.position.x;}
+        transform.position = new Vector3 (mousePosition.x,transform.position.y, transform.position.z);
+        print(mousePosition.x);
     }
 }
