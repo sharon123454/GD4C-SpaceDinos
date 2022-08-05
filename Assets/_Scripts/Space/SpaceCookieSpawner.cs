d5ca15817248090b3333331f9ee4b172cdba7d40
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-08-05 20:21:59 +0300

    lvl 3 & 4 Progress

--- Assets/_Scripts/Space/SpaceCookieSpawner.cs
+++ Assets/_Scripts/Space/SpaceCookieSpawner.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class SpaceCookieSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject cookiePrefab;
+    [SerializeField] GameObject hardCookiePrefab;
+
+    [Header("Spawn Area Objects")]
+    [SerializeField] Transform topRight;
+    [SerializeField] Transform bottomLeft;
+    [SerializeField] float spawnAmount, timeBetweenSpawn;
+    bool allowedToSpawn = false;
+
+    private void Update()
+    {
+        if (allowedToSpawn)
+            SpawnCookie();
+
+    }
+
+    private void SpawnCookie()
+    {
+        if (!SpaceController.hardMode)
+            NormalCookie();
+        else
+            HardCookie();
+
+        Invoke("ResetTimer", timeBetweenSpawn);
+    }
+
+    private void NormalCookie()
+    {
+
+    }
+
+    private void HardCookie()
+    {
+
+    }
+
+    private void ResetTimer()
+    {
+        allowedToSpawn = true;
+    }
 
 }
