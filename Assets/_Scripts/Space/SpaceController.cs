98ef0ebbd3656ebd70c1110329af78b6c8161351
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-08-05 23:48:53 +0300

    .

--- Assets/_Scripts/Space/SpaceController.cs
+++ Assets/_Scripts/Space/SpaceController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpaceController : MonoBehaviour
 {
     public static bool hardMode = false;
-    int counter = 0, cookies4NextLvl = 40;
+
+    [SerializeField] int cookies4NextLvl = 20;
+    int counter = 0;
 
     [Header("Mouse Limitations")]
     [SerializeField] float MinXMousePos = -7.4f;
@@ -13,16 +15,21 @@ public class SpaceController : MonoBehaviour
     [SerializeField] float MinYMousePos = -3.4f;
     [SerializeField] float MaxYMousePos = 3.4f;
 
+    private GameObject hardCookieInCollision;
     void Update()
     {
         Controller();
 
         if (hardMode && Input.GetMouseButtonDown(0))
-            UnlockCookie();
+            if (hardCookieInCollision)
+            {
+                Destroy(hardCookieInCollision);
+                Collect();
+            }
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         print("gay");
         if (collision.transform.CompareTag("Cookie"))
@@ -33,17 +40,20 @@ public class SpaceController : MonoBehaviour
 
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("HCookie") && hardCookieInCollision.tag != collision.transform.tag)
+        {
+            hardCookieInCollision = collision.gameObject;
+        }
+    }
+
     private void Controller()
     {
         Vector2 _worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float _clamppedX = Mathf.Clamp(_worldMousePos.x, MinXMousePos, MaxXMousePos);
         float _clamppedY = Mathf.Clamp(_worldMousePos.y, MinYMousePos, MaxYMousePos);
         transform.position = new Vector2(_clamppedX, _clamppedY);
-    }
-
-    private void UnlockCookie()
-    {
-
     }
 
     private void Collect()
--- Assets/_Scripts/Space/SpaceCookieSpawner.cs
+++ Assets/_Scripts/Space/SpaceCookieSpawner.cs
@@ -11,13 +11,12 @@ public class SpaceCookieSpawner : MonoBehaviour
     [SerializeField] Transform topRight;
     [SerializeField] Transform bottomLeft;
     [SerializeField] float spawnAmount, timeBetweenSpawn;
-    bool allowedToSpawn = false;
+    bool allowedToSpawn = true;
 
     private void Update()
     {
         if (allowedToSpawn)
             SpawnCookie();
-
     }
 
     private void SpawnCookie()
@@ -27,22 +26,43 @@ public class SpaceCookieSpawner : MonoBehaviour
         else
             HardCookie();
 
+        allowedToSpawn = false;
         Invoke("ResetTimer", timeBetweenSpawn);
     }
 
     private void NormalCookie()
     {
-
+        for (int i = 0; i < spawnAmount; i++)
+            Instantiate(cookiePrefab, RandomizeSpawnPos(), Quaternion.identity);
     }
 
     private void HardCookie()
     {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            int rand = Random.Range(0, 1);
 
+            switch (rand)
+            {
+                case 0:
+                    Instantiate(cookiePrefab, RandomizeSpawnPos(), Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(hardCookiePrefab, RandomizeSpawnPos(), Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
+    private Vector2 RandomizeSpawnPos()
+    {
+        float _randX = Random.Range(bottomLeft.transform.position.x, topRight.transform.position.x);
+        float _randy = Random.Range(bottomLeft.transform.position.y, topRight.transform.position.y);
+        return new Vector2(_randX, _randy);
     }
 
     private void ResetTimer()
     {
         allowedToSpawn = true;
     }
-
 }
