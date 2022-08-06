04c297987643053e6d64f46ec7786d9bd1293a09
benpanski <benpanski@gmail.com>
2022-08-06 16:32:05 +0300

    map

--- Assets/_Scripts/Map/MayorOfFunkyTown.cs
+++ Assets/_Scripts/Map/MayorOfFunkyTown.cs
@@ -6,25 +6,33 @@ using UnityEngine.SceneManagement;
 public class MayorOfFunkyTown : MonoBehaviour
 {
 
-
+    private void start()
+    {
+        Cursor.lockState = CursorLockMode.None ;
+    }
     public void loadEgg() 
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(0);
     }
     public void loadLeftRight()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(2);
     }
     public void loadSharon3()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(3);
     }
     public void loadSharon4()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(4);
     }
     public void loadSharon5()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SceneManager.LoadScene(5);
     }
 
