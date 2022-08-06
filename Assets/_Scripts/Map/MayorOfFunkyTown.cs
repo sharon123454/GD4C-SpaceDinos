e49f011a60094e4c6a6a1c32b8b9f8a1c2e123d2
benpanski <benpanski@gmail.com>
2022-08-06 16:04:55 +0300

    map

---
+++ Assets/_Scripts/Map/MayorOfFunkyTown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MayorOfFunkyTown : MonoBehaviour
+{
+
+
+    public void loadEgg() 
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void loadLeftRight()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void loadSharon3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void loadSharon4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void loadSharon5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+
+}
