f5e64bd1e97f0d26e48b6410c85c7133a33917b6
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-08-06 17:46:54 +0300

    .

--- Assets/_Scripts/Map/MayorOfFunkyTown.cs
+++ Assets/_Scripts/Map/MayorOfFunkyTown.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MayorOfFunkyTown : MonoBehaviour
 {
-    private void OnEnable()
+    private void Update()
     {
         Cursor.visible = true;
     }
