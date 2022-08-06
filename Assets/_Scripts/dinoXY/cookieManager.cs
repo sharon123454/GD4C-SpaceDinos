934d4872a7551d74e1bd47cf29daa80ebd6bb5ac
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-08-06 13:32:33 +0300

    Revert "gay"
    This reverts commit 5c97dd3b662b177ce9bf563a26417a2fbb6ffb98.

--- Assets/_Scripts/dinoXY/cookieManager.cs
+++ Assets/_Scripts/dinoXY/cookieManager.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public  class cookieManager : MonoBehaviour
 {
     public static int cookieCount;
+    public static int lostCookies;
+
     // Start is called before the first frame update
 }
