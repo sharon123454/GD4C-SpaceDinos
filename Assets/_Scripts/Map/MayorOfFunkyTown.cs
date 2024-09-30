d143925668dcd1747ced1dcc29af591705090ece
DSlayer <93089045+sharon123454@users.noreply.github.com>
2024-09-30 20:16:12 +0300

    Time scale issue fix

--- Assets/_Scripts/Map/MayorOfFunkyTown.cs
+++ Assets/_Scripts/Map/MayorOfFunkyTown.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class MayorOfFunkyTown : MonoBehaviour
 {
@@ -10,32 +8,35 @@ public class MayorOfFunkyTown : MonoBehaviour
         Cursor.visible = true;
     }
 
-    public void loadEgg() 
+    public void loadEgg()
     {
-
         Cursor.visible = false;
         SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
     }
     public void loadLeftRight()
     {
         Cursor.visible = false;
         SceneManager.LoadScene(2);
+        Time.timeScale = 1f;
     }
     public void loadSharon3()
     {
         Cursor.visible = false;
         SceneManager.LoadScene(3);
+        Time.timeScale = 1f;
     }
     public void loadSharon4()
     {
         Cursor.visible = false;
         SceneManager.LoadScene(4);
+        Time.timeScale = 1f;
     }
     public void loadSharon5()
     {
         Cursor.visible = false;
         SceneManager.LoadScene(5);
+        Time.timeScale = 1f;
     }
-
 
 }
